5758f6b04f76126cd5ad5ffe65631cae78c439ed
DESKTOP-R22FR0H\VuNHT <demo.app.v01@gmail.com>
2019-05-15 11:44:52 +0700

    one commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit{
+
+  constructor() { }
+  
+  ngOnInit(){
+
+  }
+
+  ngOnDestroy(){
+
+  }
+
+  ngAfterViewInit(){
+    
+  }
+ 
+}
